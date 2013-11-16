fd0833a55e242904bc0cd59ec8904e7e7cba31f7
HaWk_Rai <hawkrai2@gmail.com>
2013-11-17 00:18:28 +0300

    Correction last commit

--- MyMovies/MyMovies.DataModels/Properties/AssemblyInfo.cs
+++ MyMovies/MyMovies.DataModels/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyMovies.Data")]
+[assembly: AssemblyTitle("MyMovies.DataModels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
