a76f86f224372b20c7d5ff16e3688e1ea6a02d7d
HaWk_Rai <hawkrai2@gmail.com>
2013-11-17 00:19:39 +0300

    Correction last commit

--- MyMovies/MyMovies.DataModels/Properties/AssemblyInfo.cs
+++ MyMovies/MyMovies.DataModels/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MyMovies.Data")]
+[assembly: AssemblyProduct("MyMovies.DataModels")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
